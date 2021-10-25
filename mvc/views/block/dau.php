0f69227cc1110720dab33cb78d7c679ee37ab1df
mainhatnam <74231584+mainhatnam@users.noreply.github.com>
2021-10-25 20:44:53 +0700

    a

--- mvc/views/block/dau.php
+++ mvc/views/block/dau.php
@@ -4,6 +4,7 @@
 <head>
 
     <meta charset="utf-8">
+    <base href="http://localhost:8080/liveserver/">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
