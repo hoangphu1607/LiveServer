7e84bc570d791054c7ab783c825cd4bc3a30a449
mainhatnam <74231584+mainhatnam@users.noreply.github.com>
2021-10-31 09:44:04 +0700

    Update loadhinh.js

--- public/js/loadhinh.js
+++ public/js/loadhinh.js
@@ -1,12 +1,117 @@
-function showImage(idAnh,test) {
-    var fr=new FileReader();
-    // when image is loaded, set the src of the image where you want to display it
-    fr.onload = function(e) { target.src = this.result; };
-    src.addEventListener("change",function() {
-      // fill fr with image data    
-      fr.readAsDataURL(src.files[0]);
-      document.getElementById("duongdan").style.width = "304px";
-      document.getElementById("duongdan").style.height = "236px";
-    });
+function showImage(idAnh, duongdan) {
+  var fr = new FileReader();
+  // when image is loaded, set the src of the image where you want to display it
+  fr.onload = function (e) { duongdan.src = this.result; };
+  idAnh.addEventListener("change", function () {
+    // fill fr with image data    
+    fr.readAsDataURL(idAnh.files[0]);
+    duongdan.style.width = "70%";
+    duongdan.style.height = "auto";
+    duongdan.style.position = "relative";
+    duongdan.style.margin = "auto";
+    duongdan.style.display = "flex";
+    duongdan.style.gap = "20px";
+  });
+}
+
+
+function preview() {
+  let fileInput = document.getElementById("file-input");
+  let imageContainer = document.getElementById("images");
+  imageContainer.innerHTML = "";
+  for (i of fileInput.files) {
+    let reader = new FileReader();
+    let figure = document.createElement("figure");
+    let figCap = document.createElement("figcaption");
+    figCap.innerText = i.name;
+    figure.appendChild(figCap);
+    reader.onload = () => {
+      let img = document.createElement("img");
+      img.setAttribute("src", reader.result);
+      figure.insertBefore(img, figCap);
+    }
+    imageContainer.appendChild(figure);
+    reader.readAsDataURL(i);
   }
+}
+function preview1() {
+  let fileInput = document.getElementById("file-input");
+  let imageContainer = document.getElementById("themanh_sua");
+  imageContainer.innerHTML = "";
+  for (i of fileInput.files) {
+    let reader = new FileReader();
+    let figure = document.createElement("figure");
+    let figCap = document.createElement("figcaption");
+    figCap.innerText = i.name;
+    figure.appendChild(figCap);
+    reader.onload = () => {
+      let img = document.createElement("img");
+      img.setAttribute("src", reader.result);
+      figure.insertBefore(img, figCap);
+    }
+    imageContainer.appendChild(figure);
+    reader.readAsDataURL(i);
+  }
+}
+
+
+function show() {
+  $(document).ready(function () {
+    $(".xoa_anhct").click(function () {
+      var id_hinh = this.id;
+      var so = $(this).attr("value");
+      var test = { 'hinh': id_hinh };
+      var anh = this.parentNode.offsetParent.children[so].attributes.id.value;
+ 
+      $.post("ajax/xoa_hinhct",test,function(data){
+       data = JSON.parse(data);
+        if(data == true){ 
+            thongbao();
+           setTimeout(function() {
+            $("#"+anh).slideUp();
+           },2000);
+           setTimeout(function() {
+            $("#"+anh).remove();
+           },3000);
+  
+           // location.reload();
+
+
+        }
+      });
+
+    });
+  });
+
+
+  // $(document).ready(function () {
+  //   $(".xoa_anhct").click(function () {
+  //     var id_hinh = $(this).attr("value");
+  //     var formĐata = new FormData();
+  //     formĐata.append('hinh', id_hinh);
+  //     var ajax = new XMLHttpRequest();
+  //     ajax.onreadystatechange = function () {
+  //       if (ajax.status == 200 && ajax.readyState == 4) {
+  //         var imgPath = ajax.responseText;
+  //         $('#test').attr('src', imgPath);
+  //       }
+  //     }
+  //     ajax.open("POST", 'ajax/xoa_hinhct', true);
+  //     ajax.send(formĐata);
+  //   });
+  // });
+
+
+}
+function thongbao() {
+  Swal.fire({
+    position: 'center',
+    icon: 'success',
+    title: 'Xóa hình thành công',
+    showConfirmButton: false,
+    timer: 2000
+  })
+
+}
+
 
